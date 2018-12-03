cdbc201d54d76a5dbc8d3aa7facdbdd1a746f982
Andy Jiang <andyjiang55@yahoo.com>
2018-12-03 16:27:11 -0500

    Added Mod 1, assignment and lessons.

---
+++ Module 01/Mod01 Lessons/1.05 HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+
+/**
+ * Code used to produce "Hello, World!" to the screen. 
+ *
+ * @author Andy Jiang
+ * @version 6/6/18
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("Hello, Virtual World!");
+        System.out.println();
+        System.out.println("It's a great day for programming.");
+        
+    } // End of main() method
+    
+    
+    
+    
+} // End of class
---
+++ Module 01/Mod01 Lessons/1.05 Student Information/StudentInfoCardRevised.java
@@ -0,0 +1,75 @@
+
+/**
+ * Code that produces a Student Information Card
+ *
+ * @author Andy Jiang
+ * @version 6/6/18
+ */
+public class StudentInfoCard {
+   
+    public static void main(String[] args) {
+        
+        System.out.println("========================================");
+        System.out.println("|                                      |");
+        System.out.println("|           Student Info Card          |");
+        System.out.println("|                                      |");
+        System.out.println("========================================");
+        System.out.println();
+        System.out.println("Name: Andy Jiang");
+        System.out.println("Birthday: 5/3/2002");
+        System.out.println("Age: 16");
+        System.out.println();
+        System.out.print("School: Lawton Chiles High School");
+        System.out.println();
+        System.out.print("Grade: 10th");
+        System.out.println("     City: Tallahassee ");
+        System.out.println();
+        System.out.print("Cell Phone: (850)-371-4998");
+        System.out.println();
+        System.out.print("Email Address: andyjiang55@yahoo.com");
+        System.out.println();
+        System.out.print("Time Available: 10:00am-10:00pm");
+        System.out.println();
+        System.out.println("                 (All Week)");
+        System.out.println();
+        System.out.print("Mom's Contact Info - ");
+        System.out.println();
+        System.out.print("Cell Phone: (850)-591-7089");
+        System.out.println();
+        System.out.print("Email Address: jamesjiang1688@me.com");
+        System.out.println();
+        System.out.println();
+        System.out.print("Current Math Course: Calc AB");
+        System.out.println();
+        System.out.print("Highest Taken: Precalc BC");
+        System.out.println();
+        System.out.println();
+        System.out.print("Previous Programming Experience -");
+        System.out.println();
+        System.out.println("I started coding since I was 13 years old and");
+        System.out.println("experienced in Java, Swift, and HTML.");
+        System.out.println("Currently, I am creating a mobile app for");
+        System.out.println("the app store, using Xcode with Swift.");
+        System.out.println("I am experienced with coding in Java, as");
+        System.out.println("I have coded many projects using Eclipse, and");
+        System.out.println("am looking to learn more about programming.");
+        System.out.println("I also help make many websites for clubs");
+        System.out.println("at my school. In addition, last summer I went");
+        System.out.println("to a camp at Stanford to improve my coding");
+        System.out.println("knowledge and skills. Over this summer, I");
+        System.out.println("would love to finish my app, have deeper");
+        System.out.println("knowledge of Java, learn C++, and have");
+        System.out.println("general understanding of neutron network");
+        System.out.println("and deep learning.");
+        System.out.print("");
+        System.out.println();
+        System.out.println("I am taking APCS because I am very interested");
+        System.out.println("in Programming. I am going to major in Computer");
+        System.out.println("Science and would love to learn more about Java");
+        System.out.println("Apart from that, I truely enjoy coding and figured");
+        System.out.println("that this course would be fun to take over the");
+        System.out.println("summer!");
+            
+    }
+   
+}
